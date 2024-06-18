da40510433e96359106f8733999ed5c892c9d5ac
kadamswapnil4259@gmail.com <kadamswapnil4259@gmail.com>
2024-06-18 20:18:13 +0530

    updated bug

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
-import jsonServer from 'json-server';
-import auth from 'json-server-auth';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import  cors  from 'cors';
+import express from "express";
+import jsonServer from "json-server";
+import auth from "json-server-auth";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const server = express();
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 
-const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('./data/db.json');
+server.use('/api', router);
+server.db = router.db
 
-// Set up CORS options
-const corsOptions = {
-  origin: 'http://localhost:3000', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-// Bind the router db to the app
-app.db = router.db;
-
-// Use default middlewares (logger, static, cors, no-cache)
-app.use(cors(corsOptions)); 
-app.use(middlewares);
-app.use(auth);
-app.use(router);
-
-const port = 8000;
-app.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
+const middlewares = jsonServer.defaults()
+const rules = auth.rewriter({
+    products: 444,
+    featured_products: 444,
+    orders: 660,
+    users: 600
 });
+
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
+
+server.listen(8000);
